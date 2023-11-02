90711ebc728bdf56f7f2e13d00f191b37b42d547
Ian Pineda <ian.bravo@itsmartconnected360.com>
2023-11-01 18:35:06 -0600

    Add documentation, remove unnecessary select

--- DP_datos_tiendas.py
+++ DP_datos_tiendas.py
@@ -1,4 +1,26 @@
 from snowflake.snowpark import Session, DataFrame
 
-def datos_tiendas_dp(session: Session) -> DataFrame:
-    return session.table('SIMON_KUCHER.DOMINOS.STORE_MASTER')
+def get_datos_tiendas_dp(session: Session) -> DataFrame:
+    """
+    Genera un DataFrame de snowpark con los datos de las tiendas DP
+
+    Columnas:
+
+    (Ocupadas)
+
+    STORE_ID: Identificador unico de la tienda
+
+    REGION: Region en la que se encuentra la tienda
+
+    (Total)
+
+    TIPO_CECO, STORE_ID, TIENDA, REGION, DIRECCION, CODIGO_POSTAL, COLONIA, CIUDAD,
+    ESTADO, FORMATO, SUBFORMATO, GERENTE_DISTRITO
+    """
+    datos_tiendas_dp = (
+        session
+        .table('ANALYTICS_HUB.DATA_HUB.DS_SUCURSALES_DP')
+        .with_column_renamed('CECO', 'STORE_ID')
+    )
+
+    return datos_tiendas_dp
