933556295f5cf186089cb384ee765246fc80e4d3
Ian Pineda <ian.bravo@itsmartconnected360.com>
2023-11-02 13:46:23 -0600

    Move select to last possible moment, add docstring

--- DP_transacciones_total.py
+++ DP_transacciones_total.py
@@ -4,13 +4,37 @@ from snowflake.snowpark import Session, DataFrame
 from snowflake.snowpark.functions import lit
 
 def get_transacciones_dp_total(session: Session) -> DataFrame:
+    """
+    Genera un DataFrame de snowpark con las transacciones totales DP
+    
+    Columnas:
+
+    (Total)
+
+    EMAIL: Email del usuario
+
+    FECHA: Fecha de la transaccion
+    
+    VENTA: Cantidad de venta sin iva
+
+    ORDER_ID: Identificador de la transaccion
+
+    PHONE: Telefono del usuario
+
+    STORE_ID: Identificador de la tienda
+
+    TIENE_CUPON: Valor booleano que nos dice si la venta fue con algun cupon
+    """
+
     transacciones_olo = (
         get_transacciones_olo(session)
         .with_column('FUENTE', lit('OLO'))
+        .select(['EMAIL', 'FECHA', 'VENTA', 'ORDER_ID', 'PHONE', 'STORE_ID', 'TIENE_CUPON'])
     )
     transacciones_cloud = (
         get_transacciones_cloud(session)
         .with_column('FUENTE', lit('CLOUD'))
+        .select(['EMAIL', 'FECHA', 'VENTA', 'ORDER_ID', 'PHONE', 'STORE_ID', 'TIENE_CUPON'])
     )
 
     transacciones_total = (
